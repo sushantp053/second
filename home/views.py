85ac3434d8410e259a8b94e539896e399e89b400
Sushant Patil <sushantp053@gmail.com>
2025-07-15 22:11:39 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def home(request):
+    products  = Product.objects.all() # select * from Product
+    context = {'prods': products}
+
+    return render(request, 'home/home.html', context)
+
+def addProduct(request):
+    
+    if request.method == 'POST':
+        n = request.POST.get('name')
+        p = request.POST.get('price')
+        desc = request.POST.get('description')
+        # image = request.FILES.get('image')
+        print(n, p, desc)
+        product = Product(name=n, price=p, description=desc)
+        product.save()
+    
+    return render(request, 'home/addproduct.html')
+
